57e2c6896ba860410f0565f5321a0aa4a7c50f04
paolotiu17 <paolotiu17@gmail.com>
2020-10-10 16:08:14 +0800

    Updates

---
+++ precache-manifest.2af915adbaa0a91b101cbd334549aae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0813368b4390d66657dde0655e8927b",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "29611525709aff2c5e12",
+    "url": "/weather-app/static/css/main.cbdd1319.chunk.css"
+  },
+  {
+    "revision": "db8fca20a1196421436c",
+    "url": "/weather-app/static/js/2.73520e65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.73520e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29611525709aff2c5e12",
+    "url": "/weather-app/static/js/main.a08cc807.chunk.js"
+  },
+  {
+    "revision": "3565ad6a1d257d180595",
+    "url": "/weather-app/static/js/runtime-main.b0a5adf8.js"
+  }
+]);
